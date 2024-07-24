1cc0cdee4106b0e0d10d0dbdc6c89e87c7891ef2
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-24 17:20:03 -0300

    CSS

--- src/Heart.js
+++ src/Heart.js
@@ -1,19 +1,13 @@
+import React from 'react';
 import './style.css';
 
-function Heart(){
-  return(
-  <html>
-    <head>
-      <title>Minha Maravilhosa</title>
-      <link rel="stylesheet" type="text/css" href="style.css"></link>
-    </head>
-    <body>
-      <div class="heart"></div>
-      <h1 class="text">Eu te amo mais!</h1>
-    </body>
-  </html>
-
-  )
+function Heart() {
+  return (
+    <div className="container">
+      <div className="heart"></div>
+      <h1 className="text">Eu te amo mais!</h1>
+    </div>
+  );
 }
 
 export default Heart;
