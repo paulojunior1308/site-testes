77e10110ed343ca63a323949ffb2a3c85828ae1c
paulojunior1308 <pauloesjr2@gmail.com>
2024-07-24 17:09:51 -0300

    Site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-
+import Heart from './Heart'
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home'); // Estado para controlar a página atual
+
+  const goBack = () => {
+    setCurrentPage('home');
+  };
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'heart':
+        return <Heart goBack={goBack} />; 
+      default:
+        return (
+          <div className="bloco-lista">
+            <h1 class="text1">Se você me ama</h1>
+            <button className="bn5" onClick={() => setCurrentPage('heart')}>Clique aqui</button>
+          </div>
+        );
+    }
+  };
+
   return (
     <div className="App">
-      <a href="/" class="bn5">Clique aqui</a>
+      {renderPage()}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Heart.js
@@ -0,0 +1,19 @@
+import './style.css';
+
+function Heart(){
+  return(
+  <html>
+    <head>
+      <title>Minha Maravilhosa</title>
+      <link rel="stylesheet" type="text/css" href="style.css"></link>
+    </head>
+    <body>
+      <div class="heart"></div>
+      <h1 class="text">Eu te amo mais!</h1>
+    </body>
+  </html>
+
+  )
+}
+
+export default Heart;
