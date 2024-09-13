b7991b99ffc412049e254e8a159701efad2e0a7b
paulojunior1308 <pauloesjr2@gmail.com>
2024-09-13 11:57:21 -0300

    Site surpresa

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,22 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Welcome.css';
 import Letter from './Letter';
 import Timeline from "./Timeline";
 import Puzzle from "./Puzzle";
 import matue from "./image/matue.mp3";
 
-function Welcome(){
+function Welcome() {
   const [currentPage, setCurrentPage] = useState('home'); // Estado para controlar a página atual
 
   const goBack = () => {
     setCurrentPage('home');
   };
 
-  const playAudio = () => {
-    const audio = new Audio(matue);
-    audio.loop = true;
-    audio.play();
-  };
+  useEffect(() => {
+    if (currentPage === 'home') {
+      const audio = new Audio(matue);
+      audio.loop = true;
+      audio.play().catch(error => {
+        // Se o navegador bloquear o autoplay, o erro será capturado aqui
+        console.log("O autoplay foi bloqueado: ", error);
+      });
+    }
+  }, [currentPage]); // Toca o áudio sempre que a página 'home' for carregada
 
   const renderPage = () => {
     switch (currentPage) {
@@ -32,7 +37,7 @@ function Welcome(){
             <div className="welcome-message">
               Bem-vinda ao seu site de aniversário, minha maravilhosa! 💖
               <div className="button">
-                <button className="bn5" onClick={() => { playAudio(); setCurrentPage('timeline'); }}>
+                <button className="bn5" onClick={() => setCurrentPage('timeline')}>
                   Clique aqui
                 </button>
               </div>
